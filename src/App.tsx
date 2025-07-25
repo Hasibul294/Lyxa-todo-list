4ad738536480c054edab4ebdefa32e1419d9c5e6
Hasibul294 <hasibulhasan294@gmail.com>
2025-07-25 21:53:32 +0600

    add a new column funtionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,35 @@
+import { useState } from 'react';
+import PlusIcon from './icons/PlusIcon';
+import type { Column } from './types';
+import { generateId } from './utils/GenerateId';
+
 function App() {
+  const [columns, setColumns] = useState<Column[]>([]);
+
+  const createNewColumn = () => {
+    const addToColumn: Column = {
+      id: generateId(),
+      title: `Column ${columns.length + 1}`,
+    };
+
+    setColumns([...columns, addToColumn]);
+  };
+
+  console.log('this is column', columns);
   return (
-    <>
-      <div>
-        <h1>Kanban Style Board for LYXA</h1>
-        <button>Add Column</button>
+    <main className="py-8 px-10">
+      <div className="flex justify-between">
+        <h1 className="text-5xl font-bold bg-gradient-to-r from-blue-500 to-yellow-500 bg-clip-text text-transparent">
+          Kanban Style Board for LYXA
+        </h1>
+        <button
+          onClick={createNewColumn}
+          className="h-[60px] w-[200px] min-w-[200px] cursor-pointer rounded-lg bg-[#323334] text-white border-[#161C22] p-4 ring-emerald-300 hover:ring-2 flex justify-between items-centers"
+        >
+          Add Column <PlusIcon />
+        </button>
       </div>
-    </>
+    </main>
   );
 }
 
---
+++ src/utils/GenerateId.ts
@@ -0,0 +1,5 @@
+
+// generate a random number between 1 to 10000
+export const generateId = () => {
+    return Math.floor(Math.random() * 10001)
+}
